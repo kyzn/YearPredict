ee231ae874af9a8f8ae18948962a840f413bcfc4
Kivanc Yazan <kivancyazan@gmail.com>
2016-06-07 00:17:49 -0700

    added apt-get install commented out

--- download.sh
+++ download.sh
@@ -5,6 +5,10 @@
 # Use the following command to run:
 # . download.sh
 
+# Uncomment to install dependencies on Ubuntu machines
+# sudo apt-get update
+# sudo apt-get install git unzip python python-numpy python-pandas
+
 wget http://archive.ics.uci.edu/ml/machine-learning-databases/00203/YearPredictionMSD.txt.zip
 unzip YearPredictionMSD.txt.zip
 rm YearPredictionMSD.txt.zip
