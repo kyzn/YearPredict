5a618354f37d53083432f53aa46d1e8fe5e0a14e
Shobhit Maheshwari <shmahesh@ucsc.edu>
2016-06-09 12:35:45 -0700

    svm more training

--- SVM/README.md
+++ SVM/README.md
@@ -4,6 +4,16 @@ rm -rf *;cmake ..;make;./svmtrain;../../../libsvm/svm-scale -l -1 -u 1 -s range 
 
 ../../../libsvm/svm-predict test.scale.txt train.scale.txt.model result.txt
 
-We recieve the following results
+We receive the following results (C = 10, stopping tolerance = .1) (RBF kernel exp(-0.5|u-v|^2))
 Accuracy = 45.2809% (806/1780) (classification)
 
+
+train 5 fold cross validation
+../../../libsvm/svm-train -s 0 -c 5 -t 2 -g 0.5 -e 0.1 -v 5 train.scale.txt
+Results
+Total nSV = 9180
+Cross Validation Accuracy = 43.0275%
+
+
+Quiet mode
+../../../libsvm/svm-train -q -s 0 -c 5 -t 2 -g 0.5 -e 0.1 train.scale.txt
--- SVM/src/testHyperparam.cpp
+++ SVM/src/testHyperparam.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
+#include<cmath>
+#include<sstream>
 int main(){
-	system("../../../libsvm/svm-train -s 0 -c 5 -t 2 -g 0.5 -e 0.1 train.scale.txt");
 
-	for()
+
+	for(int i = -5; i < 16; i+=5){
+		double C =pow(2, i);
+		for (int j = -15; j < 4; j+=5){
+			double gamma = pow(2, j);
+
+			std::cout << "C: " << C << std::endl;
+			std::cout << "gamma: " << gamma << std::endl;
+
+			std::ostringstream strs;
+			strs << C;
+
+			std::ostringstream strs1;
+			strs1 << gamma;
+
+			system(("../../../libsvm/svm-train -q -s 0 -c "+strs.str()+" -t 2 -g "+strs1.str()+" -e 0.1 train.scale.txt").c_str());
+			system("../../../libsvm/svm-predict test.scale.txt train.scale.txt.model result.txt");
+
+			std::cout << std::endl;
+		}
+	}
 	return 0;
 }
