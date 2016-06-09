ae3d29547cd0bf622a047e245e52d5ea8d12548d
Shobhit Maheshwari <shmahesh@ucsc.edu>
2016-06-09 11:39:13 -0700

    saved results

--- SVM/src/main.cpp
+++ SVM/src/main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include "Transform.cpp"
 
-int main(){
-	Transform t("../CroppedData.txt");
-	t.save("train.txt", "test.txt", "ans.txt");
-	return 0;
-}
+//int main(){
+//	Transform t("../CroppedData.txt");
+//	t.save("train.txt", "test.txt", "ans.txt");
+//	return 0;
+//}
---
+++ SVM/src/testHyperparam.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+int main(){
+	system("../../../libsvm/svm-train -s 0 -c 5 -t 2 -g 0.5 -e 0.1 train.scale.txt");
+
+	for()
+	return 0;
+}
